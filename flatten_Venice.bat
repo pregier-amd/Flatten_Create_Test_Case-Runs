9e769d474e38a936166a1f70a597a5dd651cf8a8
Paul Regier <pregier@amd.com>
2024-11-20 10:19:44 -0600

    Cleanup

---
+++ flatten_Venice.bat
@@ -0,0 +1,1 @@
+python3 flatten.py --txt_flat_cyc_suite_tr ./venice/iptracker_DF.txt
--- temp.py
+++
@@ -1,2 +0,0 @@
-import commands
-commands.getstatusoutput("ssh pregier@aus-onyx-7421.amd.com /home/pregier/runner.sh echo \"Lazy Dog\"")
